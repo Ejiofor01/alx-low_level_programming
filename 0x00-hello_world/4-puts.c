936664f2ab3d8ef0eee948ea564201f360c47f80
Ejiofor01 <mac.antonoio09@gmail.com>
2022-10-13 15:38:26 -0700

    My second commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main- Serves as the entry point of the program
+ *
+ *Return: By default returns zero if errors otherwise returns non-zero value
+*/
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	returns (0);
+}
+
